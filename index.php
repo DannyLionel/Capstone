dc3e3b78bf701bdbb7383201401e2bc0b81587d6
Mohammed Hameeduddin <45569565+100587893@users.noreply.github.com>
2020-02-10 16:15:39 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Capstone Project</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+
+<body>
+  <!-- Navbar -->
+  <div class="w3-top" >
+    <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+        <a href="#home" class="w3-bar-item w3-button"><b>Capstone Project</b></a>
+        <!--Float to the right, hide in small screen -->
+        <div class="w3-right w3-hide-small">
+          <a href="#projects" class="w3-bar-item w3-button">Projects</a>
+          <a href="#about" class="w3-bar-item w3-button">About</a>
+          <a href="#contact" class="w3-bar-item w3-button">Contact</a>
+        </div>
+    </div>
+  </div>
+
+  <!--Splash Image-->
+  <header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
+    <img class="w3-image" src="https://gdurl.com/U3Y8" alt="Architecture" width="1500" height="800">
+    <div class="w3-display-middle w3-margin-top w3-center">
+      <h1 class="w3-xxlarge w3-text-white">
+        <span class="w3-padding w3-black w3-opacity-min">Ontario Shores Center for Mental Health Sciences Industrial Capstone Project</span>
+        <!--<span class="w3-hide-small w3-text-light-grey">Industrial Capstone Project</span>-->
+      </h1>
+    </div>
+  </header>
+
+  <!--Page-->
+  <div class="w3-content w3-padding" style="max-width:1564px">
+
+<!--Project Section -->
+<!--Generic Profiles -->
+<div class="w3-container w3-padding-32" id="projects">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Generic Profiles</h3>
+  </div>
+  
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Farm</div>
+        <a href='slideshow.php?profileid=farm'>  
+        <img src="https://images.unsplash.com/photo-1500076656116-558758c991c1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60" alt="House" style="width:100%">
+        </a>
+        </div>
+  </div>
+  <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Beach</div>
+         <a href='slideshow.php?profileid=beach'>                                          
+        <img src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60" alt="House" style="width:100%">
+     </a>
+     </div>
+  </div>
+  <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Winter</div>
+        <a href='slideshow.php?profileid=winter'>  
+        <img src="https://images.unsplash.com/photo-1486496146582-9ffcd0b2b2b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="House" style="width:100%">
+        </a>
+          </div>
+  </div>
+  </div>
+
+
+<!--Patient Profiles -->
+
+<div class="w3-container w3-padding-32" id="projects">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Patient Profiles</h3>
+  </div>
+  
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Jim Halpert</div>
+        <a href='slideshow.php?profileid=jim halpert'>
+        <img src="https://pbs.twimg.com/profile_images/37658942/Jim_400x400.jpg" alt="House" style="width:100%">
+        </a>
+        </div>
+  </div>
+  <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Dwight Schrute</div>
+         <a href='slideshow.php?profileid=dwight schrute'>                                          
+        <img src="https://pbs.twimg.com/profile_images/1863312401/Dwight_400x400.jpg" alt="House" style="width:100%">
+     </a>
+     </div>
+  </div>
+  <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Micheal Scott</div>
+        <a href='slideshow.php?profileid=micheal scott'>  
+        <img src="https://pbs.twimg.com/profile_images/415508301749972992/PeQwL21w_400x400.jpeg" alt="House" style="width:100%">
+        </a>
+          </div>
+  </div>
+  </div>
+
+
+
+  
+
+
+      <!-- End page content -->
+      </div>
+
+
+      <!-- Footer -->
+      <footer class="w3-center w3-black w3-padding-16">
+        <p>Made For <a href="https://www.ontarioshores.ca/" title="Ontario Shores Center for Mental Health Science" target="_blank" class="w3-hover-text-green">Ontario Shores Center for Mental Health Sciences</a></p>
+      </footer>
+</body>
+</html>
